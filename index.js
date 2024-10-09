0bf9ad2635a23681cb901428d03e4d2325e163b7
Odding07 <162929171+Odding07@users.noreply.github.com>
2024-10-09 14:57:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js')
 
 // Koneksi ke Supabase (ganti dengan detail proyek Anda)
-const supabaseUrl = 'https://your-project-url.supabase.co'
-const supabaseKey = 'your-anon-key'
+const supabaseUrl = 'https://ggfnqqsznfxlbujjpidj.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnZm5xcXN6bmZ4bGJ1ampwaWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0NDEyNDksImV4cCI6MjA0NDAxNzI0OX0.-hhUwLwXz4oygXprNZISGD430lbnEviFDVM6Il_uIKU'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Fungsi untuk menambahkan data ke tabel
